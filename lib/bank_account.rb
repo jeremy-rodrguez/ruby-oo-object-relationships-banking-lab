9ec9409201376d0bc96298ddfb04badf96b5eb70
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-19 12:58:55 -0800

    commit tree 29157132c0fec6cec8699e322eadcaafca1c5545

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
+attr_reader :name
 
+    def initialize(bank_account)
+        @bank_account = bank_account
+    end
+
+    def deposit
+    end
+
+    def display_balance
+    end
+
+    def valid?
+    end
+
+    def close_account
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  def initialize(transfer)
+    @transfer = transfer
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
 end
